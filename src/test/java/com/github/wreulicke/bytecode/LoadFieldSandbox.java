dbe35e33bb349c2a75ff5c8c003ac4c2b0ac4bb5
Wreulicke <masaya2dies@gmail.com>
2017-04-24 07:35:17 +0900

    add field instrumentation

---
+++ src/test/java/com/github/wreulicke/bytecode/LoadFieldSandbox.java
@@ -0,0 +1,107 @@
+package com.github.wreulicke.bytecode;
+
+import static net.bytebuddy.matcher.ElementMatchers.*;
+
+import java.io.IOException;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import java.lang.reflect.Modifier;
+
+import org.junit.Test;
+
+import com.github.wreulicke.bytecode.test.util.Dump;
+
+import javassist.CannotCompileException;
+import javassist.ClassPool;
+import javassist.CtClass;
+import javassist.CtMethod;
+import javassist.NotFoundException;
+import net.bytebuddy.ByteBuddy;
+import net.bytebuddy.asm.Advice;
+import net.bytebuddy.asm.Advice.DynamicValue;
+import net.bytebuddy.asm.Advice.FieldValue;
+import net.bytebuddy.asm.Advice.OnMethodEnter;
+import net.bytebuddy.asm.Advice.OnMethodExit;
+import net.bytebuddy.description.annotation.AnnotationDescription.Loadable;
+import net.bytebuddy.description.method.MethodDescription;
+import net.bytebuddy.description.method.ParameterDescription.InDefinedShape;
+import net.bytebuddy.description.type.TypeDescription;
+import net.bytebuddy.implementation.bytecode.StackManipulation;
+import net.bytebuddy.implementation.bytecode.assign.Assigner;
+import net.bytebuddy.implementation.bytecode.constant.TextConstant;
+import net.bytebuddy.matcher.ElementMatchers;
+
+public class LoadFieldSandbox {
+
+  @Test
+  public void testFieldRefWithJavassist() throws CannotCompileException, IOException, NotFoundException {
+    ClassPool pool = ClassPool.getDefault();
+    CtClass clazz = pool.get(Some.class.getName());
+    for (CtMethod method : clazz.getMethods()) {
+      if (method.getDeclaringClass()
+        .equals(clazz) && !Modifier.isNative(method.getModifiers()) && !Modifier.isAbstract(method.getModifiers())) {
+        method.insertBefore("System.out.println(\"field :\" + this.field);");
+        method.insertAfter("this.field=\"end\";" + "System.out.println(\"field :\" + this.field);");
+      }
+    }
+    byte[] bytes = clazz.toBytecode();
+    Dump.dump(bytes);
+  }
+
+  @Test
+  public void testFieldRefWithByteBuddy() throws IOException {
+    byte[] bytes = new ByteBuddy().rebase(Some.class)
+      .visit(Advice.withCustomMapping()
+        .bind(MethodName.class, new MethodNameBinder())
+        .to(ExampleAdvice.class)
+        .on(ElementMatchers.isMethod()
+          .and(isDeclaredBy(Some.class))
+          .and(not(isAbstract()))
+          .and(not(isNative()))))
+      .make()
+      .getBytes();
+    Dump.dump(bytes);
+  }
+
+  public static class ExampleAdvice {
+    @OnMethodEnter
+    public static void enter(@FieldValue(value = "field") String field) {
+      System.out.println("field :" + field);
+    }
+
+    @OnMethodExit
+    public static void exit(@FieldValue(value = "field", readOnly = false) String field) {
+      field = "end";
+      System.out.println("field :" + field);
+    }
+  }
+
+  public static class Some {
+    private String field;
+
+    public void test() {
+      System.out.println("inplemented code, field :" + field);
+    };
+  }
+
+  @Retention(RetentionPolicy.RUNTIME)
+  @Target(ElementType.PARAMETER)
+  public static @interface MethodName {
+  }
+
+  class MethodNameBinder implements DynamicValue<MethodName> {
+    @Override
+    public StackManipulation resolve(TypeDescription instrumentedType, MethodDescription instrumentedMethod, InDefinedShape target,
+      Loadable<MethodName> annotation, Assigner assigner, boolean initialized) {
+      if (target.getType()
+        .asErasure()
+        .isAssignableFrom(String.class)) {
+        String name = instrumentedMethod.getName();
+        return new TextConstant(name);
+      }
+      throw new IllegalStateException("not assignable type");
+    }
+  }
+}
