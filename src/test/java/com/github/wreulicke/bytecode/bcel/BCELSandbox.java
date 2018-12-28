3d99f1e09b6bc052e489b16ccd32300f74cd4fd1
wreulicke <saito.masaya@classmethod.jp>
2018-12-28 16:46:22 +0900

    add bcel sandbox

---
+++ src/test/java/com/github/wreulicke/bytecode/bcel/BCELSandbox.java
@@ -0,0 +1,13 @@
+package com.github.wreulicke.bytecode.bcel;
+
+import org.apache.bcel.Repository;
+import org.apache.bcel.classfile.JavaClass;
+import org.junit.Test;
+
+public class BCELSandbox {
+
+  @Test
+  public void test1() throws ClassNotFoundException {
+    JavaClass javaClass = Repository.lookupClass(BCELSandbox.class);
+  }
+}
